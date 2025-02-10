b4aa39283ae3eb2b5caa4d8037c9a3978280bdb3
svillors <tntzator9@gmail.com>
2025-02-10 21:06:17 +0500

    Improved redability

--- bot.py
+++ bot.py
@@ -5,7 +5,11 @@ from time import sleep
 from dotenv import load_dotenv
 
 
-def main(bot_token, api_token, chat_id):
+def main():
+    load_dotenv()
+    bot_token = os.environ['TG_BOT_TOKEN']
+    api_token = os.environ['DEVMAN_API_TOKEN']
+    chat_id = os.environ['TG_CHAT_ID']
     bot = telegram.Bot(token=bot_token)
     headers = {
         'Authorization': f'Token {api_token}',
@@ -18,15 +22,15 @@ def main(bot_token, api_token, chat_id):
             }
             response = requests.get('https://dvmn.org/api/long_polling/',
                                     headers=headers, params=params)
-            response_json = response.json()
+            response = response.json()
 
-            if response_json['status'] == 'timeout':
+            if response['status'] == 'timeout':
                 timestap = response.json()['timestamp_to_request']
                 continue
 
-            lesson_title = response_json['new_attempts'][0]['lesson_title']
-            lesson_url = response_json['new_attempts'][0]['lesson_url']
-            if response_json['new_attempts'][0]['is_negative']:
+            lesson_title = response['new_attempts'][0]['lesson_title']
+            lesson_url = response['new_attempts'][0]['lesson_url']
+            if response['new_attempts'][0]['is_negative']:
                 text = f'''У вас проверили работу "{lesson_title}"
 {lesson_url}\n
 К сожалению, в работе нашлись ошибки'''
@@ -36,16 +40,12 @@ def main(bot_token, api_token, chat_id):
 Преподавателю всё понравилось, можно приступать к следущему уроку'''
             bot.send_message(chat_id=chat_id,
                              text=text)
-        except requests.exceptions.ReadTimeout as e:
-            print(f'{e}')
+        except requests.exceptions.ReadTimeout:
+            pass
         except requests.exceptions.ConnectionError:
             print('connection error, reconnect...')
             sleep(5)
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    token = os.getenv('TG_BOT_TOKEN')
-    api_token = os.getenv('DEVMAN_API_TOKEN')
-    chat_id = os.getenv('TG_CHAT_ID')
-    main(token, chat_id)
+    main()
